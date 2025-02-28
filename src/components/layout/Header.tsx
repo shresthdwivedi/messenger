298fe671c6edd8556825fdcc0c15ce4596e5da48
Shresth Dwivedi <shresth0109@gmail.com>
2025-02-28 23:56:05 +0530

    add messages screen header ui

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,6 +4,10 @@ import useOtherUser from "@/hooks/useOtherUser";
 import { ConversationType } from "@/lib/types/conversationTypes";
 import { User } from "@prisma/client";
 import { useMemo } from "react";
+import Link from "next/link";
+import { IoArrowBack, IoEllipsisHorizontalSharp } from "react-icons/io5";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import Image from "next/image";
 import { Separator } from "../ui/separator";
 
 interface HeaderProps {
@@ -29,9 +33,40 @@ const Header: React.FC<HeaderProps> = ({
   }, [conversation.isGroup, conversation.users.length]);
 
   return (
-    <div className=" p-5 flex items-center gap-2 flex-row">
-      Header
-      <Separator orientation="vertical" className=""/>
+    <div className="w-full p-5 flex items-center justify-between">
+      <div className="flex flex-row items-center gap-3 ">
+        <Link
+          href={'/conversations'}
+          className="cursor-pointer lg:hidden block dark:text-neutral-400 text-neutral-800 dark:hover:text-neutral-500 hover:text-neutral-600"
+        > 
+          <IoArrowBack size={25}/>
+        </Link>
+        <div className="flex flex-row items-center">
+          <div className="flex items-center">
+            <Avatar>
+              <AvatarImage src={otherUser?.image as string} alt="user" />
+              <AvatarFallback>
+                <Image className="rounded-2xl" src={'/image.png'} alt='user-photo' width={200} height={200} />    
+              </AvatarFallback>
+              {/* <span className="absolute block rounded-full h-2.5 w-2.5 bg-green-500 top-0 right-0 ring-2 ring-white"/> */}
+            </Avatar>
+            <Separator orientation="vertical" className="h-8 mx-4" />
+          </div>
+          <div className="flex flex-col">
+            <p className="font-bold text-lg dark:text-neutral-400 text-neutral-800">
+              {otherUser?.name}
+            </p>
+            <p className="text-sm dark:text-neutral-400 text-neutral-800">
+              {statusText}
+            </p>
+          </div>
+        </div>
+      </div>
+      <IoEllipsisHorizontalSharp 
+        size={25} 
+        className="cursor-pointer dark:text-neutral-400 text-neutral-800 dark:hover:text-neutral-500 hover:text-neutral-600"
+        onClick={() => {}}
+      />
     </div>
   )
 }
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -6,7 +6,7 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 import { cn } from "@/lib/utils"
 
 const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(
   (
@@ -18,7 +18,7 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-neutral-200 dark:bg-neutral-800",
+        "shrink-0 bg-neutral-400 dark:bg-neutral-800",
         orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
         className
       )}
